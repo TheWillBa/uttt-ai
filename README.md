d8bd81bc565b2252a5bf62fdebffa594f444274b
TheWillBa <wbabincsak@gmail.com>
2021-09-24 11:31:28 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+A Python Version of the Ultimate Tic Tac Toe game to experiment with AI techniques
+
+Current dependencies:
+numpy
---
+++ program.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+
+#  Constants
+NO_MARKER = 0
+PLAYER1_MARKER = 1
+PLAYER2_MARKER = 2
+
+main_board = np.zeros((9, 9))
+
+print(main_board)
